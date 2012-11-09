88ec03e07b1aedaa29bb2f4b50b743569eee50ef
David Valentine <david.valentine+github@gmail.com>
2012-11-09 13:53:36 -0800

    Initial Commit

---
+++ CuahsiCvLs/Client/UserCode/TermDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class TermDetail
+    {
+        partial void Term_Loaded(bool succeeded)
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Term);
+        }
+
+        partial void Term_Changed()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Term);
+        }
+
+        partial void TermDetail_Saved()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Term);
+        }
+    }
+}
